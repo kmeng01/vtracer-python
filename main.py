0e07048ad1001768b1a3832dc440303e1a1b40bd
Kevin Meng <kevin.meng.01@gmail.com>
2022-08-03 21:28:53 -0700

    Backup

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from rustpy_vtracer import trace
 import cv2
 
-img = cv2.imread("a.jpg")
+img = cv2.imread("dog.jpg")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA).tolist()
 
 svg = trace(img)
-with open("test.svg", "w") as f:
+with open("out.svg", "w") as f:
     f.write(svg)
